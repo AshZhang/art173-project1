851fe934d15d712f048f4a0c5232ca8f13f053c3
AshZhang <ashley.w.zhang@gmail.com>
2020-09-26 13:32:56 -0700

    Added audio distortion

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const CHEST_URL = "https://res.cloudinary.com/dw3ipuew9/image/upload/v1600798655/art%20173/proj1image/chest_lwyure.png";
 const COMPRESS_CHANGE = 0.05;
-const NOISE_VOLUME_CHANGE = 0.15;
-const MEM_VOLUME_CHANGE = 0.12;
-const DISTORT_LEVEL = 400;
+const NOISE_VOLUME_CHANGE = 0.05;
+const MAX_NOISE_VOLUME = 0.1;
+
+
+const DISTORT_LEVEL = 5000;
+const DISTORT_LEVEL_CHANGE = 0.25;
 const MIN_OPACITY = 0.35;
 const OPACITY_CHANGE = 0.2;
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const distortion = audioCtx.createWaveShaper();
-distortion.curve = makeDistortionCurve(400);
-distortion.oversample = '4x';
+const MIN_MEM_VOLUME = 0.025;
+const MEM_VOLUME_DECAY = 1/3;
+
 const noise = new Howl({ src: "https://res.cloudinary.com/dw3ipuew9/video/upload/v1600795651/art%20173/proj1sound/noise_zmj80z.wav", loop: true, preload: true });
 const ambience = new Howl({ src: "https://res.cloudinary.com/dw3ipuew9/video/upload/v1600842259/art%20173/proj1sound/ambience_hc8qmb.m4a", loop: true, preload: true });
+const distortion = Howler.ctx.createWaveShaper();
+distortion.connect(Howler.ctx.destination);
+Howler.masterGain.connect(distortion);
 const container = document.body.querySelector(".container");
 const imageSources = [
     "https://res.cloudinary.com/dw3ipuew9/image/upload/v1600799345/art%20173/proj1image/0_j2lzar.jpg",
@@ -66,7 +71,8 @@ async function initializeElems() {
             sound: memSound,
             noiseVolume: 0,
             compressLevel: 0.1,
-            opacity: 1
+            opacity: 1,
+            distortLevel: 0
         });
     }
     await initializeBlobs();
@@ -77,12 +83,17 @@ async function initializeElems() {
 function stopMemory(memory) {
     memory.active = false;
     memory.sound.stop();
-    memory.sound.volume(Math.max(0.1, memory.sound.volume() * (2 / 3)));
+    memory.sound.volume(Math.max(MIN_MEM_VOLUME, memory.sound.volume() * MEM_VOLUME_DECAY));
     noise.stop();
+
+    distortion.curve = makeDistortionCurve(0);
+    Howler.volume(1);
+
     ambience.play();
     compressImg(memory);
     memory.compressLevel = Math.max(0, memory.compressLevel - COMPRESS_CHANGE);
-    memory.noiseVolume = Math.min(1, memory.noiseVolume + NOISE_VOLUME_CHANGE);
+    memory.noiseVolume = Math.min(MAX_NOISE_VOLUME, memory.noiseVolume + NOISE_VOLUME_CHANGE);
+    memory.distortLevel = Math.min(1, memory.distortLevel + DISTORT_LEVEL_CHANGE);
     memory.opacity = Math.max(MIN_OPACITY, memory.opacity - OPACITY_CHANGE);
     memory.elem.src = CHEST_URL;
     memory.elem.style.setProperty("opacity", memory.opacity);
@@ -99,10 +110,12 @@ function openMemory(i) {
         memory.elem.src = memory.memorySrc;
         memory.elem.style.setProperty("opacity", 1);
         ambience.stop();
+        if (memory.noiseVolume != 0) { Howler.volume(0.05); }
+        distortion.curve = makeDistortionCurve(memory.distortLevel * DISTORT_LEVEL);
+
         memory.sound.play();
         noise.play();
         memory.active = true;
-
     } else {
         stopMemory(memory);
     }
@@ -121,6 +134,7 @@ function compressImg(memory) {
     });
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WaveShaperNode
 function makeDistortionCurve(amount) {
     var k = typeof amount === 'number' ? amount : 50,
         n_samples = 44100,
